b401ccf20e724581ff66ffe6f3c1593396c13aed
mustafa.dagdelen <mustafa.dagdelen@bilgeadam.com>
2019-03-13 13:33:15 +0300

    initial commit

---
+++ Lab01/config.tf
@@ -0,0 +1,27 @@
+provider "docker" {
+  host = "tcp://192.168.99.100:2376"
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:1.11-alpine"
+}
+
+
+resource "docker_container" "nginx-server" {
+  count = 2
+  name = "nginx-server-${count.index+1}"
+  image = "${docker_image.nginx.latest}"
+  ports {
+      internal = 80
+  }
+
+  volumes {
+    container_path = "/usr/share/nginx/html"
+    host_path = "/volume"
+    read_only = true
+  }
+}
+
+
+
+
